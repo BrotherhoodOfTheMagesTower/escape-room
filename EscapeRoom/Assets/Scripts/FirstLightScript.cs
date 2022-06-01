af7af72335391a5b1273a17c18e3f218a36d18cd
wikikrajcer <wiki.krajcer@gmail.com>
2022-06-01 15:52:55 +0200

    ﻿Implemented full functionality for game board

---
+++ EscapeRoom/Assets/Scripts/FirstLightScript.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class FirstLightScript : MonoBehaviour
+{
+	bool inside = false;
+
+	void Start()
+	{
+		inside = false;
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.gameObject.tag == "FirstLight")
+		{
+			col.gameObject.GetComponent<Renderer>().material.color = new Color(0, 1, 0);
+			inside = !inside;
+		}
+	}
+
+	void OnTriggerExit(Collider col)
+	{
+		if (col.gameObject.tag == "FirstLight")
+		{
+			inside = !inside;
+		}
+	}
+
+}
+
