599ce4f629a9e82520429ba84321a7719005529a
Dom3r <kubsler123@gmail.com>
2022-06-21 23:23:41 +0200

    Added all tips and key to room.

---
+++ EscapeRoom/Assets/Room2_Things/SCRIPTS/ShowTipBook.cs
@@ -0,0 +1,87 @@
+using StarterAssets;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowTipBook : MonoBehaviour
+{
+
+
+    [SerializeField]
+    GameObject panel;
+    [SerializeField]
+    bool showPanel = false;
+    FirstPersonController FPC;
+    bool inside = false;
+    Text description;
+
+    StarterAssets.StarterAssetsInputs mouseLook;
+
+    void OnMouseDrag()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = mousePos;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        FPC = GameObject.Find("PlayerCapsule").GetComponent<FirstPersonController>();
+        description = GameObject.Find("TextBook").GetComponent<Text>();
+        panel.SetActive(showPanel);
+        inside = false;
+        mouseLook = GameObject.Find("PlayerCapsule").GetComponent<StarterAssets.StarterAssetsInputs>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            openPanel();
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            closePanel();
+        }
+    }
+    private void openPanel()
+    {
+        showPanel = true;
+        panel.SetActive(showPanel);
+        mouseLook.cursorLocked = !mouseLook.cursorLocked;
+        FPC.stopStartCameraRotationUpdate = false;
+        if (!mouseLook.cursorLocked)
+        {
+            mouseLook.cursorLocked = true;
+            mouseLook.cursorInputForLook = false;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+
+    private void closePanel()
+    {
+        FPC.stopStartCameraRotationUpdate = true;
+        showPanel = false;
+        panel.SetActive(showPanel);
+        mouseLook.cursorLocked = true;
+        mouseLook.cursorInputForLook = true;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+}
+
+
+
+
