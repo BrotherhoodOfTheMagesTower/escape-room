adbc532fbb1d5f71356b6c8cebd0e4b2fb21a6de
Marek Kawalski <marek.kawalski@gmail.com>
2022-06-27 12:17:02 +0200

    Add icons to hammer, key and fuel tank. Remove unneccesary items while leaving the room.

--- EscapeRoom/Assets/EscapeGarage.cs
+++ EscapeRoom/Assets/EscapeGarage.cs
@@ -5,11 +5,16 @@ public class EscapeGarage : MonoBehaviour
     [SerializeField] sceneController sc;
     [SerializeField] string sceneName;
     [SerializeField] int keyId;
+    [SerializeField] int hammerId;
+    [SerializeField] int fuelId;
     [SerializeField] ToastController toastController;
     void OnTriggerEnter(Collider other)
     {
         if (InventoryManager.Items.Find(k => k.id == keyId))
         {
+            //Remove unnecessary items before moving back to main scene
+            InventoryManager.Items.Remove(InventoryManager.Items.Find(h => h.id == hammerId));
+            InventoryManager.Items.Remove(InventoryManager.Items.Find(f => f.id == fuelId));
             sc.changeSceneTo(sceneName);
         }
         else
